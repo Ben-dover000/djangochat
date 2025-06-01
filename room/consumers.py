80ddf59cf6555e7b031d176e5d7f56c11675967b
Leandro <leandro@gmail.com>
2025-06-01 03:25:55 +0100

    PAP

--- room/consumers.py
+++ room/consumers.py
@@ -1,10 +1,6 @@
 import json
-
-from django.contrib.auth.models import User
 from channels.generic.websocket import AsyncWebsocketConsumer
 from asgiref.sync import sync_to_async
-
-from .models import Room, Message
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
@@ -15,26 +11,22 @@ class ChatConsumer(AsyncWebsocketConsumer):
             self.room_group_name,
             self.channel_name
         )
-
         await self.accept()
 
-    async def disconnect(self):
+    async def disconnect(self, close_code):
         await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
         )
 
-    # Receive message from WebSocket
     async def receive(self, text_data):
         data = json.loads(text_data)
-        print(data)
         message = data['message']
         username = data['username']
         room = data['room']
 
         await self.save_message(username, room, message)
 
-        # Send message to room group
         await self.channel_layer.group_send(
             self.room_group_name,
             {
@@ -44,12 +36,10 @@ class ChatConsumer(AsyncWebsocketConsumer):
             }
         )
 
-    # Receive message from room group
     async def chat_message(self, event):
         message = event['message']
         username = event['username']
 
-        # Send message to WebSocket
         await self.send(text_data=json.dumps({
             'message': message,
             'username': username
@@ -57,7 +47,8 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
     @sync_to_async
     def save_message(self, username, room, message):
+        from django.contrib.auth.models import User
+        from .models import Room, Message
         user = User.objects.get(username=username)
         room = Room.objects.get(slug=room)
-
         Message.objects.create(user=user, room=room, content=message)
