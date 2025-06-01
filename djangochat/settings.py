e73d8c132f20f395828b89cda6c71f5fd86c4594
Leandro <leandro@gmail.com>
2025-06-01 16:38:36 +0100

    aaasc

--- djangochat/settings.py
+++ djangochat/settings.py
@@ -13,14 +13,23 @@ import os
 from pathlib import Path
 import dj_database_url
 import cloudinary
-import os
+
+import cloudinary
 
 cloudinary.config(
-    cloud_name=os.environ.get('CLOUDINARY_CLOUD_NAME'),
-    api_key=os.environ.get('CLOUDINARY_API_KEY'),
-    api_secret=os.environ.get('CLOUDINARY_API_SECRET'),
-    secure=True
+    cloud_name='dg8zlfzw3',
+    api_key='434447942919425',
+    api_secret='IQIDBU6LR_-lu4hdtNYy5fwLwv0',
+    secure=True,
 )
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': 'dg8zlfzw3',
+    'API_KEY': '434447942919425',
+    'API_SECRET': 'IQIDBU6LR_-lu4hdtNYy5fwLwv0',
+}
 
 # ─── Build paths inside the project like this: BASE_DIR / 'subdir' ────────────────────
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -52,13 +61,6 @@ INSTALLED_APPS = [
     'room',
 ]
 
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-
-CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': 'dg8zlfzw3',
-    'API_KEY': '434447942919425',
-    'API_SECRET': 'IQIDBU6LR_-lu4hdtNYy5fwLwv0',
-}
 
 
 MIDDLEWARE = [
