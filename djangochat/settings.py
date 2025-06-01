741aa2fafd6cb2ea0f5542001ea8d9da9f7ed04e
Leandro <leandro@gmail.com>
2025-06-01 05:13:54 +0100

    t111111

--- djangochat/settings.py
+++ djangochat/settings.py
@@ -157,7 +157,7 @@ STATIC_ROOT = str(BASE_DIR / 'staticfiles')
 # ─── Media files (uploads) ────────────────────────────────────────────────────────────
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # ─── Login / Logout redirections ──────────────────────────────────────────────────────
