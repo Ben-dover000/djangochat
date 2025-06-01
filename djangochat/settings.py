a721f8ab24802e348b27800180866a5a73bb31f4
Leandro <leandro@gmail.com>
2025-06-01 16:30:40 +0100

    AAA

--- djangochat/settings.py
+++ djangochat/settings.py
@@ -12,7 +12,15 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os
 from pathlib import Path
 import dj_database_url
+import cloudinary
+import os
 
+cloudinary.config(
+    cloud_name=os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    api_key=os.environ.get('CLOUDINARY_API_KEY'),
+    api_secret=os.environ.get('CLOUDINARY_API_SECRET'),
+    secure=True
+)
 
 # ─── Build paths inside the project like this: BASE_DIR / 'subdir' ────────────────────
 BASE_DIR = Path(__file__).resolve().parent.parent
